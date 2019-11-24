5573fca378ba4b8335055e05257cade42ad68fdd
jjunu <junukim.dev@gmail.com>
2019-11-24 13:27:40 +0900

    commit

--- src/app.js
+++ src/app.js
@@ -1,24 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const override = require('method-override');
+const express = require("express");
+const cors = require("cors");
+const override = require("method-override");
 
 const app = express();
-const apiRouter = require('./api');
-const webSocket = require('./socket');
-const db = require('./models');
-db.sequelize.sync();
+const webSocket = require("./socket");
 
-app.set('port', 3000);
+app.set("port", 3000);
 app.use(cors());
 app.use(override());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use('/api', apiRouter);
-
-const server = app.listen(app.get('port'), () => {
-    console.log(`server is listening on ${app.get('port')} port`);
-})
+const server = app.listen(app.get("port"), () => {
+  console.log(`server is listening on ${app.get("port")} port`);
+});
 
 webSocket(server, app);
